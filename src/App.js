ed578160f94dee10f5310a40d1844957e5372c81
Ingpawat <Ingpawat.k@gmail.com>
2022-10-09 19:26:37 +0700

    next -> add search button

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
             <div className="inputWrap flex justify-start items-center flex-col w-[60%] h-[800px] mt-[2%] rounded-[25px] bg-gradient-to-r from-cyan-300 to-blue-400 z-0  ">
 
                 <input
-                    className=" w-[900px] h-[55px] m-0 mt-[10%] border-[3px] border-[#005c9d] z-10 rounded-[20px]
+                    className=" w-[80%] h-[55px] m-0 mt-[10%] border-[3px] border-[#005c9d] z-10 rounded-[20px]
                     hover:border-[5px] hover:border-[#555555]
                     "
                     placeholder='City Here!'
@@ -54,21 +54,21 @@ function App() {
                     ) : (
                         <div className='contentBox w-[90%] h-[90%] flex flex-row z-10 rounded-[25px]'>
 
-                            <div className='flex flex-col items-center justify-center w-[50%] h-[100%]   '>
+                            <div className='cityName flex flex-col items-center justify-center w-[50%] h-[100%]   '>
                                 <a className='text-[60px] w-[80%] h-[25%] text-center bg-blue-200 rounded-md hover:bg-white ' href='#1'>
                                     {data.name}
                                 </a>
                             </div>
 
-                            <div className=' w-[100%] h-[100%] flex items-center'>
+                            <div className='cityTemp w-[100%] h-[100%] flex items-center'>
 
                                 <div className='flex flex-col text-center w-[50%] h-[30%]   '>
-                                    <p className=''>{data.sys.country}</p>
+                                    <p className='sys'>{data.sys.country}</p>
                                     <p className='text-[60px]'>{Math.round(data.main.temp - 273.15)}°C</p>
                                     <p className=''>{data.weather[0].main}</p>
                                 </div>
 
-                                <div className='flex flex-col item-center justify-center text-center w-[50%] h-[100%] text-[25px]  '>
+                                <div className='cityDetail flex flex-col item-center justify-center text-center w-[50%] h-[100%] text-[25px]  '>
 
                                     <div className='mb-[10%] w-[90%] bg-blue-200 rounded-md hover:bg-white '>
                                         <p className=''>FEELS LIKE</p>
